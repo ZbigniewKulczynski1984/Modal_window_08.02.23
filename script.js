cd90d2d863b64a47e8f79d200d21ecb94b72959e
Zbigniew Kulczński <z-kulczynski@o2.pl>
2023-02-09 10:41:40 +0100

    modified

--- script.js
+++ script.js
@@ -6,14 +6,16 @@ const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnsOpenModal);
 
 for (let i = 0; i < btnsOpenModal.length; i++)
-	btnsOpenModal[i].addEventListener('click', 
-		function() {
-			console.log('Button clicked');
-            modal.classList.remove('hidden')
-            overlay.classList.remove('hidden')
-		},
-	);
-    btnCloseModal.addEventListener('click', function() {
-        modal.classList.add('hidden')
-        overlay.classList.add('hidden')
-    })
+	btnsOpenModal[i].addEventListener('click', function () {
+		console.log('Button clicked');
+		modal.classList.remove('hidden');
+		overlay.classList.remove('hidden');
+	});
+
+const closeModal = function () {
+	modal.classList.add('hidden');
+	overlay.classList.add('hidden');
+};
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
